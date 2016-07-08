02a84eebb1db7561f6f2f986f1cc528365d16405
Laura Morillo-Velarde <lauramorillo@users.noreply.github.com>
2016-07-08 18:10:16 +0200

    Removed logs to check if that also causes some leaks

--- index.js
+++ index.js
@@ -4,28 +4,14 @@ const datastore = gcloud.datastore();
 function getUrl(urlId) {
   console.log('Getting a new url');
   const key = datastore.key(['Url', urlId]);
-  datastore.get(key, (err, urlEntity) => {
-    if (!err) {
-      console.log('Worked!');
-      console.log(urlEntity);
-    } else {
-      console.log(err);
-    }
-  });
+  datastore.get(key, (err, urlEntity) => {});
 }
 
 function findUrls() {
   console.log('Searching urls');
   const key = datastore.key(['Url', 'www.my_url.com']);
   const query = datastore.createQuery('Url').filter('__key__', key);
-  datastore.runQuery(query, (err, entities, info) =>{
-    if (!err) {
-      console.log('Worked!');
-      console.log(entities);
-    } else {
-      console.log(err);
-    }
-  });
+  datastore.runQuery(query, (err, entities, info) =>{});
 }
 
 setInterval(() => {
