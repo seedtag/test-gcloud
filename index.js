1307590993af8103c2a1606f6bb44845784b8b66
Laura Morillo-Velarde <lauramorillo@users.noreply.github.com>
2016-07-08 17:50:28 +0200

    Added search by key to the example project

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ function getUrl(urlId) {
 
 function findUrls() {
   console.log('Searching urls');
-  const query = datastore.createQuery('Url');
+  const key = datastore.key(['Url', 'www.my_url.com']);
+  const query = datastore.createQuery('Url').filter('__key__', key);
   datastore.runQuery(query, (err, entities, info) =>{
     if (!err) {
       console.log('Worked!');
