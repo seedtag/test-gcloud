232945c80773c3f6106bab8a20e7e4b28123f6a6
Laura Morillo-Velarde <lauramorillo@users.noreply.github.com>
2016-07-08 13:25:39 +0200

    Simplified the example

--- index.js
+++ index.js
@@ -1,29 +1,18 @@
 const gcloud = require('gcloud')();
 const datastore = gcloud.datastore();
 
-function get(urlId) {
-  return new Promise((resolve, reject) => {
-    const key = datastore.key(['SherlockUrl', urlId]);
-    datastore.get(key, (err, urlEntity) => {
-      if (!err) {
-        resolve(urlEntity);
-      } else {
-        reject(err);
-      }
-    });
+function getUrl(urlId) {
+  console.log('Getting a new url');
+  const key = datastore.key(['Url', urlId]);
+  datastore.get(key, (err, urlEntity) => {
+    if (!err) {
+      console.log('Worked!');
+    } else {
+      console.log(err);
+    }
   });
 }
 
-function request() {
-  console.info('Requesting...')
-  get('urlForTest').then((url) => {
-    console.info('Request completed');
-    request();
-  }).catch(err => {
-    console.error('Ups, it failed!');
-    console.error(err);
-    request();
-  })
-}
-
-request();
+setInterval(() => {
+  getUrl('http://not_existing.com');
+}, 50);
