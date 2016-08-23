8bbc00624525bdd47fad48b5aa602e48e51158fc
Laura Morillo-Velarde Rodríguez <laura.morillovelarde@gmail.com>
2016-08-23 22:49:53 +0200

    Changed to use SherlockUrl for the example too.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const shortid = require('shortid');
 function getUrl(urlId) {
   const timeKey = `Time_${shortid.generate()}`;
   console.time(timeKey);
-  const key = datastore.key(['UrlTest', urlId]);
+  const key = datastore.key(['SherlockUrl', urlId]);
   datastore.get(key, (err, urlEntity) => {
     if (err) {
       console.log(err);
@@ -16,26 +16,10 @@ function getUrl(urlId) {
 
 function findUrls() {
   console.log('Searching urls');
-  const key = datastore.key(['Url', 'www.my_url.com']);
+  const key = datastore.key(['SherlockUrl', 'www.my_url.com']);
   const query = datastore.createQuery('UrlTest').filter('__key__', key);
   datastore.runQuery(query, (err, entities, info) =>{});
 }
-
-console.time('Creating url')
-const key = datastore.key(['Url', 'www.test-url.com']);
-const entity = {
-    key: key,
-    data: {url: 'www.test-url.com'}
-  };
-datastore.save(
-    entity,
-    function (err) {
-      if (err) {
-        console.log(err);
-      }
-      console.timeEnd('Creating url')
-    }
-  );
 
 setInterval(() => {
   getUrl('www.test-url.com');
